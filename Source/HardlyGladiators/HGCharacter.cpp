514aa987d0ebaa9b804a4a6a4ee292be3ef1176c
Jeasonfire <jeasonfire@gmail.com>
2016-08-25 19:07:28 +0300

    Added a CPP layer between ACharacter and the BP.

---
+++ Source/HardlyGladiators/HGCharacter.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2016  Jens Pitkanen & Aleksi Talarmo
+
+#include "HardlyGladiators.h"
+#include "HGCharacter.h"
+
+AHGCharacter::AHGCharacter() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AHGCharacter::BeginPlay() {
+	Super::BeginPlay();	
+}
+
+void AHGCharacter::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+}
---
+++ Source/HardlyGladiators/HGCharacter.h
@@ -0,0 +1,21 @@
+﻿// Copyright (C) 2016  Jens Pitkanen & Aleksi Talarmo
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "HGCharacter.generated.h"
+
+UCLASS()
+class HARDLYGLADIATORS_API AHGCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AHGCharacter();
+
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaSeconds) override;
+
+    UFUNCTION(BlueprintImplementableEvent, Category="Character")
+    void OnAction();
+};
