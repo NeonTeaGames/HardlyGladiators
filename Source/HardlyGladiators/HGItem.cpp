644582bbc35e19eb3786e3ec420e94373eef280d
Allexit <horde98@gmail.com>
2016-08-28 02:53:05 +0300

    Added generating sword

---
+++ Source/HardlyGladiators/HGItem.cpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2016  Jens Pitkanen & Aleksi Talarmo
+
+#include "HardlyGladiators.h"
+#include "HGItem.h"
+
+AHGItem::AHGItem() {
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AHGItem::BeginPlay() {
+	Super::BeginPlay();
+	
+}
+
+void AHGItem::Tick( float DeltaTime ) {
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Source/HardlyGladiators/HGItem.h
@@ -0,0 +1,19 @@
+// Copyright (C) 2016  Jens Pitkanen & Aleksi Talarmo
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "HGItem.generated.h"
+
+UCLASS()
+class HARDLYGLADIATORS_API AHGItem : public AActor {
+	GENERATED_BODY()
+	
+public:	
+	AHGItem();
+	virtual void BeginPlay() override;
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
