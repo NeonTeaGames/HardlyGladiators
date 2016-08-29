1ab5c40424b529fd440268466ffeef5985503bf6
Allexit <horde98@gmail.com>
2016-08-29 03:10:45 +0300

    Added Vitality and Blood systems for HGCharacter
    Blood system still need tweaking but I need sleep so not today buddy

--- Source/HardlyGladiators/HGCharacter.cpp
+++ Source/HardlyGladiators/HGCharacter.cpp
@@ -5,12 +5,117 @@
 
 AHGCharacter::AHGCharacter() {
 	PrimaryActorTick.bCanEverTick = true;
+
+	this->Vitalities = TMap<FName, float>();
+	this->CriticalBodyParts = TArray<FName>();
+	this->Blood = 5;
 }
 
 void AHGCharacter::BeginPlay() {
-	Super::BeginPlay();	
+	Super::BeginPlay();
+	this->ResetVitalitiesToCurrentSkeleton();
 }
 
 void AHGCharacter::Tick(float DeltaTime) {
     Super::Tick(DeltaTime);
+
+	if (this->Dead) {
+		return;
+	}
+
+	this->Blood += 0.01 * DeltaTime; // Regen 1 cl blood every second
+	
+	for (auto &Vitality : this->Vitalities) {
+		float Damage = this->MaxVitality - Vitality.Value;
+		float Lost = Damage / 2000 * DeltaTime;
+		if (this->CriticalBodyParts.Contains(Vitality.Key)) {
+			Lost *= 2;
+		}
+		this->Blood -= Lost;
+	}
+
+	if (!this->Fainted && this->Blood <= 1.5) {
+		this->Fainted = true;
+		this->OnFaint();
+	}
+	else if (this->Fainted && this->Blood >= 3) {
+		this->Fainted = false;
+		this->OnWake();
+	}
+
+	if (this->Blood <= 0) {
+		this->Die();
+	}
+
+	this->Blood = fmin(this->Blood, this->MaxBlood);
+}
+
+// Set which body parts are critical
+void AHGCharacter::setCriticalBodyParts(TArray<FName> CriticalBodyParts) {
+	this->CriticalBodyParts = CriticalBodyParts;
+}
+
+// Deal damage to a specified body part
+void AHGCharacter::DealDamageToBodyPart(FName BodyPartName, float Damage) {
+	float *Current = this->Vitalities.Find(BodyPartName);
+	if (Current != nullptr) {
+		float Result = *Current - Damage;
+		this->Vitalities.Add(BodyPartName, *Current - Damage);
+		UE_LOG(LogTemp, Warning, TEXT("Bodypart '%s' now only has %f health left :("), *BodyPartName.ToString(), Result);
+		this->OnBodyPartTakeDamage(BodyPartName, Damage);
+
+		if (Result <= 0) {
+			this->OnBodyPartDie(BodyPartName, Damage);
+
+			if (this->CriticalBodyParts.Contains(BodyPartName)) {
+				this->Die();
+			}
+		}
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("I don't have a bone called %s!"), *BodyPartName.ToString());
+	}
+}
+
+// Get vitality of the specified body part
+float AHGCharacter::GetBodyPartVitality(FName BodyPartName) {
+	float *Current = this->Vitalities.Find(BodyPartName);
+	if (Current != nullptr) {
+		return *Current;
+	}
+	return -1;
+}
+
+// Remove body part from the vitalities
+void AHGCharacter::RemoveBodyPartFromVitalities(FName BodyPartName) {
+	this->Vitalities.Remove(BodyPartName);
+}
+
+// Add the body part to vitalities or set it's vitality to 100
+float AHGCharacter::AddOrRefreshVitality(FName BodyPartName) {
+	this->Vitalities.Add(BodyPartName, this->MaxVitality);
+	return 100;
+}
+
+// Remove all vitalities and add new ones from current bones with default health of 100
+void AHGCharacter::ResetVitalitiesToCurrentSkeleton() {
+	this->Vitalities = TMap<FName, float>();
+
+	USkeletalMeshComponent *SkeletalMesh = Cast<USkeletalMeshComponent>
+		(this->GetComponentByClass(USkeletalMeshComponent::StaticClass()));
+	TArray<FName> Names;
+	SkeletalMesh->GetBoneNames(Names);
+
+	for (int Counter = 0; Counter < Names.Num(); Counter++) {
+		this->Vitalities.Add(Names[Counter], this->MaxVitality);
+	}
+}
+
+float AHGCharacter::GetCurrentBlood() {
+	return this->Blood;
+}
+
+void AHGCharacter::Die() {
+	this->Dead = true;
+	this->OnDie();
 }
--- Source/HardlyGladiators/HGCharacter.h
+++ Source/HardlyGladiators/HGCharacter.h
@@ -6,8 +6,7 @@
 #include "HGCharacter.generated.h"
 
 UCLASS()
-class HARDLYGLADIATORS_API AHGCharacter : public ACharacter
-{
+class HARDLYGLADIATORS_API AHGCharacter : public ACharacter {
 	GENERATED_BODY()
 
 public:
@@ -18,4 +17,53 @@ public:
 
     UFUNCTION(BlueprintImplementableEvent, Category="Character")
     void OnAction(bool LeftHandedAction);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Character")
+	void OnBodyPartTakeDamage(FName BodyPartName, float Damage);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Character")
+	void OnBodyPartDie(FName BodyPartName, float FatalDamage);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Character")
+	void OnFaint();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Character")
+	void OnWake();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Character")
+	void OnDie();
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	void setCriticalBodyParts(TArray<FName> CriticalBodyParts);
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	void DealDamageToBodyPart(FName BodyPartName, float Damage);
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	float GetBodyPartVitality(FName BodyPartName);
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	void RemoveBodyPartFromVitalities(FName BodyPartName);
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	float AddOrRefreshVitality(FName BodyPartName);
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	void ResetVitalitiesToCurrentSkeleton();
+
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	float GetCurrentBlood();
+
+private:
+	TMap<FName, float> Vitalities;
+	TArray<FName> CriticalBodyParts;
+	float Blood;
+
+	bool Fainted = false;
+	bool Dead = false;
+
+	static const int MaxVitality = 100;
+	static const int MaxBlood = 5;
+
+	void Die();
 };
